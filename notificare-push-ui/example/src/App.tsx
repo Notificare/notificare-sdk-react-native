fc2ee86a85c10f241f43f331d25e4a15f2fa2771
Helder Pinhal <helder@notifica.re>
2021-06-07 14:15:30 +0100

    chore: add lifecycle examples to push-ui sample

--- notificare-push-ui/example/src/App.tsx
+++ notificare-push-ui/example/src/App.tsx
@@ -44,12 +44,52 @@ export default function App() {
         console.log('=== NOTIFICATION OPENED ===');
         console.log(JSON.stringify(notification, null, 2));
 
-        if (notification.actions.length > 0) {
-          await NotificarePushUI.presentAction(notification, notification.actions[0]);
-          return;
-        }
+        // if (notification.actions.length > 0) {
+        //   await NotificarePushUI.presentAction(notification, notification.actions[0]);
+        //   return;
+        // }
 
         await NotificarePushUI.presentNotification(notification);
+      }),
+      NotificarePushUI.onNotificationWillPresent((notification) => {
+        console.log('=== NOTIFICATION WILL PRESENT ===');
+        console.log(JSON.stringify(notification));
+      }),
+      NotificarePushUI.onNotificationPresented((notification) => {
+        console.log('=== NOTIFICATION PRESENTED ===');
+        console.log(JSON.stringify(notification));
+      }),
+      NotificarePushUI.onNotificationFinishedPresenting((notification) => {
+        console.log('=== NOTIFICATION FINISHED PRESENTING ===');
+        console.log(JSON.stringify(notification));
+      }),
+      NotificarePushUI.onNotificationFailedToPresent((notification) => {
+        console.log('=== NOTIFICATION FAILED TO PRESENT ===');
+        console.log(JSON.stringify(notification));
+      }),
+      NotificarePushUI.onNotificationUrlClicked((data) => {
+        console.log('=== NOTIFICATION URL CLICKED ===');
+        console.log(JSON.stringify(data));
+      }),
+      NotificarePushUI.onActionWillExecute((data) => {
+        console.log('=== ACTION WILL EXECUTE ===');
+        console.log(JSON.stringify(data));
+      }),
+      NotificarePushUI.onActionExecuted((data) => {
+        console.log('=== ACTION EXECUTED ===');
+        console.log(JSON.stringify(data));
+      }),
+      NotificarePushUI.onActionNotExecuted((data) => {
+        console.log('=== ACTION NOT EXECUTED ===');
+        console.log(JSON.stringify(data));
+      }),
+      NotificarePushUI.onActionFailedToExecute((data) => {
+        console.log('=== ACTION FAILED TO EXECUTE ===');
+        console.log(JSON.stringify(data));
+      }),
+      NotificarePushUI.onCustomActionReceived((url) => {
+        console.log('=== CUSTOM ACTION RECEIVED ===');
+        console.log(JSON.stringify(url));
       }),
     ];
 
