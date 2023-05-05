7d64b0f1b245638fa57d8a9421fd1262259ae271
Helder Pinhal <helder@notifica.re>
2023-05-05 16:31:33 +0100

    feat(authentication): remove auth module

--- packages/react-native-notificare-authentication/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NotificareAuthentication } from './notificare-authentication';
-
-export * from './notificare-authentication';
-
-export * from './models/notificare-user';
-export * from './models/notificare-user-preference';
-export * from './models/notificare-user-segment';
-
-export default NotificareAuthentication;
