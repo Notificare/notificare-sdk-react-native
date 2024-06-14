3fd90155f980fa3c968c2358663e1d560bb5a9e9
yevheniismirnov <yevhenii.smirnov@outlook.com>
2024-06-14 11:25:13 +0200

    refactor(monetize): drop support for monetize

--- packages/react-native-notificare-monetize/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { NotificareMonetize } from './notificare-monetize';
-
-export * from './notificare-monetize';
-
-export * from './models/notificare-product';
-export * from './models/notificare-purchase';
-
-export default NotificareMonetize;
