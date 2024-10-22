f68853ce2dc78f740e62d486f110f2bfb3c30532
yevheniismirnov <yevhenii.smirnov@outlook.com>
2024-10-22 15:15:18 +0100

    refactor(core): ensure correct import syntax is used

--- packages/react-native-notificare/ios/NotificareModule.mm
+++ packages/react-native-notificare/ios/NotificareModule.mm
@@ -1,9 +1,10 @@
 #import "NotificareModule.h"
-#import "CoreData/NSManagedObject.h"
-#import "NotificareKit/NotificareKit-Swift.h"
+
+#import <CoreData/NSManagedObject.h>
+#import <NotificareKit/NotificareKit-Swift.h>
 
 #if __has_include(<react_native_notificare/react_native_notificare-Swift.h>)
-#import <react_native_notificare/react_native_notificare-Swift.h>
+#import "react_native_notificare/react_native_notificare-Swift.h"
 #else
 #import "react_native_notificare-Swift.h"
 #endif
